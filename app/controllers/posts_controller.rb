90f6dc80158f1b2639fc3bffa5f1ea4c4bbe0538
saacha <takoyakidaisukisaki@gmail.com>
2021-12-23 16:28:10 +0900

    update2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,33 @@
 class PostsController < ApplicationController
     
     def show
+        @message = "Welcome Home!"
+        @posts = Post.all
     end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to :home
+
+        else
+            render :new
+        end
+
+    end
+
+    private
+
+      def post_params
+        params.require(:post).permit(:content, :user_name)
+      end
+
+
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/home',to:'posts#show'
+  get '/new',to:'posts#new'
+  post '/posts',to:'posts#create'
 end
